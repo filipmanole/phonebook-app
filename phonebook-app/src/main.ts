284c16109fafec77f68fef88e0a9051bcc797278
Filip Manole <filip.manole@gmail.com>
2022-10-19 11:33:51 +0300

    clean up

--- phonebook-app/src/main.ts
+++ phonebook-app/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { setupCorsOrigin } from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // app.enableCors({
-  //   credentials: true,
-  //   origin: setupCorsOrigin,
-  // });
 
   app.enableCors();
 
